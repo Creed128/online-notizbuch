bf74e0cc2d081f70a1238f8abab26a3f8b553daf
hajarbakir <161323069+Hajar-Bakir@users.noreply.github.com>
2024-04-30 12:20:48 +0200

    update

--- src/Komponenten/Home.js
+++ src/Komponenten/Home.js
@@ -8,8 +8,9 @@ const Home = () => {
     <div className="home-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="container vh-100 d-flex justify-content-center align-items-center">
         <div className="main-content text-center bg-light p-5 rounded shadow">
-          <h1 className="display-4">Welcome to Mein Notizbuch!</h1>
-          <p className="lead">Macht deine besten Notizen hier.</p>
+          <h4 className="display-4">Welcome to Mein Notizbuch!</h4>
+
+          <p className="lead">Mach deine besten Notizen.</p>
           <button className="btn btn-primary mt-4">Create New Note</button>
         </div>
       </div>
