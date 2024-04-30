9009885d912ba1d8017f92ac3f7da80dcd143c68
Credo Ngoukeng <credongoukeng@gmail.com>
2024-04-30 18:15:42 +0200

    update navigation

--- src/Komponenten/Navigation/Navigation.js
+++ src/Komponenten/Navigation/Navigation.js
@@ -1,13 +1,29 @@
 // src/Komponenten/Navigation/Navigation.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
 import './Navigation.css';
 
 const Navigation = () => {
+  const { user, logout } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login'); // Rediriger vers la page de connexion après la déconnexion
+  };
+
   return (
     <nav className="navigation-bar">
       <Link to="/home" className="nav-link">Home</Link>
-      <Link to="/login" className="nav-link">Login</Link>
+      {user ? (
+        <>
+          <button onClick={handleLogout} className="nav-link">logout</button>
+          {/* Retiré le bouton "Notiz erstellen" */}
+        </>
+      ) : (
+        <Link to="/login" className="nav-link">Login</Link>
+      )}
     </nav>
   );
 };
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,13 +1,20 @@
+// src/contexts/UserContext.js
 import React, { createContext, useState } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // Initialisation à null
+  const [user, setUser] = useState(null); // Démarrez avec null pour indiquer aucun utilisateur connecté par défaut
+
+  // Fonction pour déconnecter l'utilisateur
+  const logout = () => {
+    setUser(null); // Réinitialise l'utilisateur à null lors de la déconnexion
+  };
 
   const value = {
     user,
     setUser,
+    logout, // Fournir la fonction de déconnexion via le contexte
   };
 
   return (
