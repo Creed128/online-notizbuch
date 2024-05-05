03587fb319883c0ed9a629f6c7ed807a08df9bc5
Credo Ngoukeng <credongoukeng@gmail.com>
2024-05-05 19:43:43 +0200

    update noteModel for private and public note

--- src/server/models/noteModel.js
+++ src/server/models/noteModel.js
@@ -1,8 +1,12 @@
+// src/server/models/noteModel.js
 const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    content: { type: String, required: true }
+    content: { type: String, required: true },
+    isPublic: { type: Boolean, default: true },
+    owner: { type: String, required: true }, 
+    createdAt: { type: Date, default: Date.now }, 
 });
 
 const Note = mongoose.model('Note', noteSchema);
