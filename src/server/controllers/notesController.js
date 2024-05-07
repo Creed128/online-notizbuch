e2f8559aa158dbd1ad8411347739747898e4d2e1
Credo Ngoukeng <credongoukeng@gmail.com>
2024-05-07 14:35:37 +0200

    change in notecontrollers

--- src/server/controllers/notesController.js
+++ src/server/controllers/notesController.js
@@ -19,21 +19,25 @@ exports.getAllNotes = async (req, res) => {
 
 // Créer une nouvelle note
 exports.createNote = async (req, res) => {
-    try {
-      const newNote = new Note({
-        title: req.body.title,
-        content: req.body.content,
-        isPublic: req.body.isPublic || true,
-        owner: req.body.owner,
-        createdAt: req.body.createdAt || new Date() // Ajouter cette ligne pour utiliser la date actuelle si non spécifiée
-      });
-      await newNote.save();
-      res.status(201).json(newNote);
-    } catch (error) {
-      res.status(500).send('Erreur lors de la création de la note : ' + error.message);
-    }
-  };
-  
+  const { title, content, isPublic, owner } = req.body;
+  if (!title || !content || !owner) {
+    return res.status(400).json({ message: 'Veuillez fournir un titre, un contenu et un propriétaire' });
+  }
+
+  try {
+    const newNote = new Note({
+      title,
+      content,
+      isPublic: isPublic ?? true, // Si `isPublic` n'est pas fourni, la valeur par défaut est `true`
+      owner,
+      createdAt: new Date()
+    });
+    await newNote.save();
+    res.status(201).json(newNote);
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la création de la note', error: error.message });
+  }
+};
 
 // Mettre à jour une note
 exports.updateNote = async (req, res) => {
@@ -50,21 +54,19 @@ exports.updateNote = async (req, res) => {
     }
     res.status(200).json(updatedNote);
   } catch (error) {
-    res.status(500).json({ message: 'Erreur lors de la création de la note', error: error.message });
+    res.status(500).send('Erreur lors de la mise à jour de la note : ' + error.message);
   }
 };
 
+// Supprimer une note
+exports.deleteNote = async (req, res) => {
   try {
-    const newNote = new Note({
-      title,
-      content,
-      isPublic: isPublic ?? true, // Si `isPublic` n'est pas fourni, la valeur par défaut est `true`
-      owner,
-      createdAt: new Date()
-    });
-    await newNote.save();
-    res.status(201).json(newNote);
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote) {
+      return res.status(404).send('Note non trouvée');
+    }
+    res.status(200).send('Note supprimée avec succès');
   } catch (error) {
-    res.status(500).json({ message: 'Erreur lors de la création de la note', error: error.message });
+    res.status(500).send('Erreur lors de la suppression de la note : ' + error.message);
   }
 };
